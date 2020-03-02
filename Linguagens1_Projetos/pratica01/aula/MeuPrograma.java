cd25cb8f34aa874387a145529daf726befc86a59
CAIO CRUZ ALFONSO GARCIA <15.01580-7@maua.br>
2020-03-02 11:26:18 -0300

    meu aqrquivo java do do primeiro programa

---
+++ Linguagens1_Projetos/pratica01/aula/MeuPrograma.java
@@ -0,0 +1,8 @@
+class MeuPrograma {
+    public static void main(String[] args){
+        // Mensagem de saida do sistema
+        System.out.println("Olá mundo!");
+        System.out.println("Estou Programando no modo Conan.:)");
+
+    }
+}
